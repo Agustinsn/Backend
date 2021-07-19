f82d4e34e44396ce6a3d8272d1520d919eaf97f0
Agustinsn <agustinasn95@gmail.com>
2021-07-19 18:33:57 -0500

    Reto1

---
+++ RetoSemanal1/Reto1.py
@@ -0,0 +1,12 @@
+
+import time
+name=input("Ingrese su nombre: ")
+age=int(input("Ingrese su edad: "))
+hour=int(time.strftime("%H"))
+if age>=18:
+    print(f"No tienes ninguna responsabilidad {name}!")
+else:
+    if hour>6:
+        print(f"{name} debes dormir!")
+    else:
+        print(f"{name} debes hacer tus tareas!")
