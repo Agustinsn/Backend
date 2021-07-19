2881a53d900c18784523b13df1b7f75315a081d9
Agustinsn <agustinasn95@gmail.com>
2021-07-19 18:34:48 -0500

    .

--- RetoSemanal1/Reto1.py
+++ RetoSemanal1/Reto1.py
@@ -7,6 +7,6 @@ if age>=18:
     print(f"No tienes ninguna responsabilidad {name}!")
 else:
     if hour>6:
-        print(f"{name} debes dormir!")
+        print(f"{name} debes ir a dormir!")
     else:
         print(f"{name} debes hacer tus tareas!")
